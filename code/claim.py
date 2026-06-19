c47d1ef70a7df8d80bd0f84698e3483bc796fbaa
Jack Garratt <jackgarratt25@gmail.com>
2026-06-19 14:13:08 +0100

    feat: add new attributes to Claim class

--- code/claim.py
+++ code/claim.py
@@ -5,6 +5,16 @@ class Claim:
         self.user_claim = user_claim
         self.claim_object = claim_object
         self.previous_claims_context = previous_claims_context
+        self.evidence_standard_met = None
+        self.evidence_standard_met_reason = None                           
+        self.risk_flags = None                            
+        self.issue_type = None                           
+        self.object_part = None                         
+        self.claim_status = None                        
+        self.claim_status_justification = None            
+        self.supporting_image_ids = None       
+        self.valid_image = None  
+        self.severity = None
 
     def __repr__(self):
         return f"<Claim user_id={self.user_id} object={self.claim_object}>"
